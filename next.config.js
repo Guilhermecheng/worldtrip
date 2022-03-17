6587e0490b3f277a8d4623fd70f1f8e8b068bba7
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2022-03-17 01:21:28 -0300

    vercel fix comment

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   nextConfig,
   images: {
     loader: 'imgix',
-    // path: 'http://localhost:3000/',
+    // path: 'http://localhost:3000/', // this path for svg use in dev mode
     path: '/',
     domains: ['prismic.io', 'images.prismic.io']
   },
